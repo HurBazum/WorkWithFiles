077006081a587280d295f5f1e8dce238ea0958fc
Anton Spiridonov <hoorbazoom@gmail.com>
2023-01-17 01:24:21 +0700

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Security.AccessControl;
+
+namespace WorkWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = @"C:\\testFolders";
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
+                if (directoryInfo.Exists)
+                { 
+                    DeleteOldFoldersAndFiles(directoryInfo);
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.HelpLink);
+            }
+        }
+        static void DeleteOldFoldersAndFiles(DirectoryInfo directoryInfo)
+        {
+            DateTime lastAccessAt;
+            TimeSpan deleteTime = TimeSpan.FromMinutes(30);
+            //удаляются объекты в папке
+            var directoryFiles = directoryInfo.GetFiles();
+            var directoryFolders = directoryInfo.GetDirectories();
+            try
+            {
+                //сканирует файлы в папке, удаляет те, к которым обращались более 30 минут назад
+                foreach (var file in directoryFiles)
+                {
+                    lastAccessAt = file.LastAccessTime;
+                    if (DateTime.Now - lastAccessAt > deleteTime)
+                    {
+                        Console.WriteLine($"файл {file} время последнего доступа {lastAccessAt}");
+                        Console.WriteLine($"прошло больше {deleteTime.Minutes} минут c последнего доступа, - {file} надо удалять");
+                        file.Delete();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"файл {file} время последнего доступа {lastAccessAt}");
+                        Console.WriteLine($"прошло меньше {deleteTime.Minutes} минут c последнего доступа," +
+                            $" - {file} не надо удалять");
+                    }
+                }
+                //сканирует папки, удаляет те, к которым обращались более 30 минут назад, со всем содержимым
+                //если это не так переходит к предыдущему блоку и сканирует все файлы в текущей директории
+                foreach (var directory in directoryFolders)
+                {
+                    lastAccessAt = directory.LastAccessTime;
+                    if (DateTime.Now - lastAccessAt > deleteTime)
+                    {
+                        Console.WriteLine($"Папка {directory} время последнего доступа {lastAccessAt}");
+                        Console.WriteLine($"прошло больше {deleteTime.Minutes} минут c последнего доступа, - {directory} надо удалять");
+                        directory.Delete(true);
+                        continue;//для перехода к следующей директории после удаления данной, т.к. DeleteFiles(directory) расположен ниже
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Папка {directory} время последнего доступа {lastAccessAt}");
+                        Console.WriteLine($"прошло меньше {deleteTime.Minutes} минут c последнего доступа," +
+                            $" - {directory} не надо удалять");
+                    }
+                    DeleteOldFoldersAndFiles(directory);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.HelpLink);
+            }
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using static System.Net.WebRequestMethods;
+
+namespace WorkWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // file:///C:/GOG%20Games/Gothic%202%20Gold/
+                // %20 - указывается вместо пробела в ссылке
+                //"file:///C:/Games/srlzbl/Pharaoh/Audio"
+                // file:///C:/GOG%20Games/Gothic%202%20Gold/saves/current
+                string s = string.Empty;
+                long directorySize = 0;
+                Console.WriteLine("Введите урл папки(вместо пробелов вводится - %20): ");
+                s = Console.ReadLine();
+                ///<summary> 
+                ///Проверка верна ли введена ссылка e.g. "file:///C:/" - ок, "file:///C:" - не ок,
+                ///при неверном вводе выбрасывается исключение
+                ///</summary>
+                if(!Regex.IsMatch(s, @"file:\/\/\/\w{1}\:[\/\w\/]+"))
+                {
+                    throw new Exception("Неверно указана ссылка!\nНеобходимо начинать с file:// и название диска заключать в - /.../");
+                }
+                string newS = new string(s.Substring(8));
+                if(Directory.Exists(newS)) 
+                    Console.WriteLine((GetSize(s, ref directorySize) + " - размер данной директории."));
+                else
+                    throw new DirectoryNotFoundException();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.HelpLink);
+            }
+        }
+        static long GetSize(string s, ref long size)
+        {
+            Uri uri = new Uri(s);
+            DirectoryInfo di = new DirectoryInfo(uri.LocalPath);
+            try
+            {
+                if (di.Exists)
+                {
+                    //скаладывает длины файлов в основной папке
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                        size += file.Length;
+                    }
+                    //складывает длины файлов во вложенных папках, если они есть
+                    DirectoryInfo[] directoryInfos = di.GetDirectories();
+                    foreach (DirectoryInfo directoryInfo in directoryInfos)
+                    {
+                        GetSize(directoryInfo.FullName, ref size);
+                    }
+                }
+                else
+                {
+                    throw new DirectoryNotFoundException();
+                }
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.HelpLink);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.HelpLink);
+            }
+            return size;
+        }
+    }
+}
+
